ae82aff298d00122492894302c49dbf7751b144f
ricardocaiuba <ricardo.caiuba@gmail.com>
2021-01-16 13:11:33 -0300

    Rota e controller de perfil

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import appointmentsRouter from "@modules/appointments/infra/http/routes/appointm
 import usersRouter from "@modules/users/infra/http/routes/users.routes";
 import sesionsRouter from "@modules/users/infra/http/routes/sessions.routes";
 import passwordRouter from "@modules/users/infra/http/routes/password.routes";
+import profilerRouter from "@modules/users/infra/http/routes/profile.routes";
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use("/appointments", appointmentsRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sesionsRouter);
 routes.use("/password", passwordRouter);
+routes.use("/profile", profilerRouter);
 
 export default routes;
