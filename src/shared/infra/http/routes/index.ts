303fb6ba942d203f07a24d7dd77da172d3835d79
ricardocaiuba <ricardo.caiuba@gmail.com>
2021-02-13 15:52:28 -0300

    Listagem de prestadores

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import appointmentsRouter from "@modules/appointments/infra/http/routes/appointments.routes";
+import providersRouter from "@modules/appointments/infra/http/routes/providers.routes";
 import usersRouter from "@modules/users/infra/http/routes/users.routes";
 import sesionsRouter from "@modules/users/infra/http/routes/sessions.routes";
 import passwordRouter from "@modules/users/infra/http/routes/password.routes";
@@ -8,6 +9,7 @@ import profilerRouter from "@modules/users/infra/http/routes/profile.routes";
 const routes = Router();
 
 routes.use("/appointments", appointmentsRouter);
+routes.use("/providers", providersRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sesionsRouter);
 routes.use("/password", passwordRouter);
