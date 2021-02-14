80806146dedb18b83648eb7a299012eec77a596d
ricardocaiuba <ricardo.caiuba@gmail.com>
2021-02-14 14:22:21 -0300

    Excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -17,12 +17,17 @@ describe("ListProviderDayAvailability", () => {
   it("should be able to list the day availability from provider", async () => {
     await fakeAppointmentsRepository.create({
       provider_id: "user_id",
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: "user_id",
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      const custonDate = new Date(2020, 4, 20, 11);
+      return custonDate.getTime();
     });
 
     const availabily = await listProviderDayAvailability.execute({
@@ -35,9 +40,13 @@ describe("ListProviderDayAvailability", () => {
     expect(availabily).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
       ])
     );
   });
